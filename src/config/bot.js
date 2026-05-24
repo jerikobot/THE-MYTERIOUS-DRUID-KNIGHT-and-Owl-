bd62f65af0a5979a4eaf55cf4d8abba77e5b5e42
jerikobot <asrieldrawings@gmail.com>
2026-05-24 15:56:27 -0600

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -543,13 +543,5 @@ export function getRandomColor() {
 }
 
 export default botConfig;
-client.on("message" , msg => {
-    var Mensajes = ["hola", "callate 100 años, w", "ok."];
-    var aleatorio = Math.floor(Math.random()*(Mensajes.length));
-    if(msg.content.startsWith("hola")){
-       msg.channel.send(Mensajes[aleatorio]);
-      }
-  });
-
 
 
